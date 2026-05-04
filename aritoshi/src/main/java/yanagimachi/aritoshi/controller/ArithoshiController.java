5ced5c1b5ba41cd25295835048499bed581f6c9b
nagiarr <nagiarr@yahoo.co.jp>
2026-05-05 02:17:22 +0900

    add index

--- aritoshi/src/main/java/yanagimachi/aritoshi/controller/ArithoshiController.java
+++ aritoshi/src/main/java/yanagimachi/aritoshi/controller/ArithoshiController.java
@@ -1,8 +1,9 @@
+package yanagimachi.aritoshi.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ArithoshiController {
     @GetMapping("/index")
     public String hello() {
