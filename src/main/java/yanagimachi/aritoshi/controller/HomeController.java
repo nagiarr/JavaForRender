a79511b638e8a601efc5155c145014b84bdf5063
nagiarr <nagiarr@yahoo.co.jp>
2026-05-14 15:06:28 +0900

    change Controller for thymeleaf

--- src/main/java/yanagimachi/aritoshi/controller/HomeController.java
+++ src/main/java/yanagimachi/aritoshi/controller/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ArithoshiController {
+public class HomeController {
     @GetMapping("/")
-    public String hello() {
+    public String home() {
         return "index";
     }
 }
