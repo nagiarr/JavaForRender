1e49424e4770e4d9a31fe32dbec4cf78f29c8d18
nagiarr <nagiarr@yahoo.co.jp>
2026-05-14 21:34:10 +0900

    add delete task

--- src/main/java/yanagimachi/aritoshi/controller/TodoController.java
+++ src/main/java/yanagimachi/aritoshi/controller/TodoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import yanagimachi.aritoshi.entity.Todo;
 import yanagimachi.aritoshi.repository.TodoRepository;
@@ -23,6 +25,13 @@ public class TodoController {
         model.addAttribute("todos", todos);
 
         return "index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTodo(@PathVariable Long id) {
+        todoRepository.deleteById(id);
+
+        return "redirect:/";
     }
 
     @PostMapping("/add")
